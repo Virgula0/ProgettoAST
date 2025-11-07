1777da3f81bb939c5384f1a6c09e7d6394d285ce
Angelo Rosa <angelo.rosa@protonmail.com>
2025-11-07 16:49:02 +0100

    Repository using mongodb for user model (#13)
    * setup mongodb test containers, ci will fail because of coverage
    
    * TDD: implement save user method
    
    * TDD user is null
    
    * TDD for ull fields
    
    * TDD: testRegistrationToken
    
    * TDD: implement findUserById
    
    * refactor findUserById
    
    * TDD working on: findUserByUsernameAndPassword
    
    * tdd: add test case for testing null values
    
    * refactor findUserByUsernameAndPassword
    
    * TDD: findUserByUsername implementation
    
    * refactor findUserByUsername
    
    * removing useles package private method and user userCollection as done until now
    
    * apply sonarcube suggestions
    
    * add doc test

---
+++ com.rosa.angelo.progetto.ast/src/main/java/com/rosa/angelo/progetto/ast/repository/UserMongoRepository.java
@@ -0,0 +1,76 @@
+package com.rosa.angelo.progetto.ast.repository;
+
+import java.util.Objects;
+import java.util.stream.StreamSupport;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.rosa.angelo.progetto.ast.model.User;
+
+public class UserMongoRepository implements UserRepository {
+
+	public static final String IMAGE = System.getProperty("mongo.image", "mongo");
+	public static final String VERSION = System.getProperty("mongo.version", "4.4.3");
+	public static final int PORT = Integer.parseInt(System.getProperty("mongo.port", "27017"));
+
+	public static final String AST_DB_NAME = "ast";
+	public static final String USER_COLLECTION_NAME = "users";
+	private MongoCollection<Document> userCollection;
+
+	public static final String ID_KEY = "id";
+	public static final String USERNAME_KEY = "username";
+	public static final String PASSWORD_KEY = "password";
+
+	public static final String REGISTRATION_TOKEN = "validToken";
+
+	public UserMongoRepository(MongoClient client, String databaseName, String collectionName) {
+		userCollection = client.getDatabase(databaseName).getCollection(collectionName);
+	}
+
+	public UserMongoRepository(MongoClient client) {
+		this(client, AST_DB_NAME, USER_COLLECTION_NAME);
+	}
+
+	@Override
+	public void save(User user) {
+		if (user == null || user.getUsername() == null || user.getPassword() == null) {
+			return;
+		}
+		userCollection.insertOne(new Document().append(ID_KEY, user.getId()).append(USERNAME_KEY, user.getUsername())
+				.append(PASSWORD_KEY, user.getPassword()));
+	}
+
+	@Override
+	public String getRegistrationToken() {
+		/*
+		 * This is just a simplification for the purpose of the project Registration
+		 * tokens should be single disposable after each registration
+		 */
+		return REGISTRATION_TOKEN;
+	}
+
+	private User documentToUser(Document doc) {
+		return new User(doc.getString(USERNAME_KEY), doc.getString(PASSWORD_KEY), doc.getInteger(ID_KEY));
+	}
+
+	@Override
+	public User findUserById(int id) {
+		return StreamSupport.stream(userCollection.find().spliterator(), false).map(this::documentToUser)
+				.filter(x -> Objects.equals(x.getId(), id)).findFirst().orElse(null);
+	}
+
+	@Override
+	public User findUserByUsernameAndPassword(String username, String password) {
+		return StreamSupport.stream(userCollection.find().spliterator(), false).map(this::documentToUser)
+				.filter(x -> Objects.equals(x.getUsername(), username) && Objects.equals(password, x.getPassword()))
+				.findFirst().orElse(null);
+	}
+
+	@Override
+	public User findUserByUsername(String username) {
+		return StreamSupport.stream(userCollection.find().spliterator(), false).map(this::documentToUser)
+				.filter(x -> Objects.equals(x.getUsername(), username)).findFirst().orElse(null);
+	}
+}
---
+++ com.rosa.angelo.progetto.ast/src/test/java/com/rosa/angelo/progetto/ast/repository/TestUserMongoDBRepository.java
@@ -0,0 +1,204 @@
+package com.rosa.angelo.progetto.ast.repository;
+
+import static com.mongodb.client.model.Filters.eq;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+
+import org.bson.Document;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.rosa.angelo.progetto.ast.model.User;
+
+public class TestUserMongoDBRepository {
+
+	@SuppressWarnings({ "rawtypes", "resource" })
+	@ClassRule
+	public static final GenericContainer mongo = new GenericContainer(
+			UserMongoRepository.IMAGE + ":" + UserMongoRepository.VERSION).withExposedPorts(UserMongoRepository.PORT);
+
+	private static MongoClient client;
+	private static MongoDatabase database;
+
+	private UserMongoRepository userRepository;
+	private MongoCollection<Document> userCollection;
+
+	private static final String TEST_USERNAME = "TEST_USERNAME";
+	private static final String TEST_PASSWORD = "TEST_PASSWORD";
+
+	@BeforeClass
+	public static void beforeClass() {
+		client = new MongoClient(new ServerAddress(mongo.getHost(), mongo.getMappedPort(UserMongoRepository.PORT)));
+		database = client.getDatabase(UserMongoRepository.AST_DB_NAME);
+	}
+
+	@Before
+	public void setup() {
+		// make sure we always start with a clean database
+		database.drop();
+		userRepository = new UserMongoRepository(client);
+		userCollection = database.getCollection(UserMongoRepository.USER_COLLECTION_NAME);
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		client.close();
+	}
+
+	private void addTestUserToDatabase(int id, String username, String password) {
+		userCollection
+				.insertOne(new Document().append("id", id).append("username", username).append("password", password));
+	}
+
+	@Test
+	public void testSaveANewUserSuccesfully() {
+		User user = new User(TEST_USERNAME, TEST_PASSWORD, 1);
+		userRepository.save(user);
+
+		assertThat(userCollection.find(eq("id", 1))
+				.map(doc -> new User(doc.getString("username"), doc.getString("password"), doc.getInteger("id")))
+				.into(new ArrayList<>())).containsExactly(new User(TEST_USERNAME, TEST_PASSWORD, 1));
+	}
+
+	@Test
+	public void testSaveANewUserIsNull() {
+		User user = null;
+		userRepository.save(user);
+
+		assertThat(userCollection.find()
+				.map(doc -> new User(doc.getString("username"), doc.getString("password"), doc.getInteger("id")))
+				.into(new ArrayList<>())).isEmpty();
+	}
+
+	@Test
+	public void testSaveFieldsOfUserAreNullExceptForID() {
+		User userWithUsernameNull = new User(null, TEST_PASSWORD, 1);
+		User userWithPasswordNull = new User(TEST_USERNAME, null, 1);
+		User bothNull = new User(null, null, 1);
+
+		userRepository.save(userWithUsernameNull);
+
+		assertThat(userCollection.find()
+				.map(doc -> new User(doc.getString("username"), doc.getString("password"), doc.getInteger("id")))
+				.into(new ArrayList<>())).isEmpty();
+
+		userRepository.save(userWithPasswordNull);
+
+		assertThat(userCollection.find()
+				.map(doc -> new User(doc.getString("username"), doc.getString("password"), doc.getInteger("id")))
+				.into(new ArrayList<>())).isEmpty();
+
+		userRepository.save(bothNull);
+
+		assertThat(userCollection.find()
+				.map(doc -> new User(doc.getString("username"), doc.getString("password"), doc.getInteger("id")))
+				.into(new ArrayList<>())).isEmpty();
+	}
+
+	@Test
+	public void testRegistrationToken() {
+		assertThat(userRepository.getRegistrationToken()).isEqualTo("validToken");
+	}
+
+	@Test
+	public void testFindUserByIdWhenUserExists() {
+		addTestUserToDatabase(1, TEST_USERNAME, TEST_PASSWORD);
+		addTestUserToDatabase(2, TEST_USERNAME, TEST_PASSWORD);
+
+		User user = userRepository.findUserById(2);
+
+		assertThat(user).isEqualTo(new User(TEST_USERNAME, TEST_PASSWORD, 2));
+	}
+
+	@Test
+	public void testFindUserByIdWhenUserDoesNotExistsAndCollectionIsEmpty() {
+		User user = userRepository.findUserById(1);
+
+		assertThat(user).isNull();
+	}
+
+	@Test
+	public void testfindUserByUsernameAndPasswordOnExistingUser() {
+		String username = "user1";
+		String password = "password1";
+		addTestUserToDatabase(1, username, password);
+		String username2 = "user2";
+		String password2 = "password2";
+		addTestUserToDatabase(2, username2, password2);
+
+		User user = userRepository.findUserByUsernameAndPassword(username2, password2);
+
+		assertThat(user).isEqualTo(new User(username2, password2, 2));
+		assertThat(user.getPassword()).isEqualTo(password2);
+		assertThat(userRepository.findUserByUsernameAndPassword("", "")).isNull();
+	}
+
+	@Test
+	public void testfindUserByUsernameAndPasswordWhenUsernameOrPasswordIsWrong() {
+		String username = "user1";
+		String password = "password1";
+		addTestUserToDatabase(1, username, password);
+		String username2 = "user2";
+		String password2 = "password2";
+		addTestUserToDatabase(2, username2, password2);
+
+		assertThat(userRepository.findUserByUsernameAndPassword("wrongusername", password2)).isNull();
+		assertThat(userRepository.findUserByUsernameAndPassword(username2, "wrongPassword")).isNull();
+		assertThat(userRepository.findUserByUsernameAndPassword("wrongUsername", "wrongPassword")).isNull();
+
+	}
+
+	@Test
+	public void testfindUserByUsernameAndPasswordWithFuzzedNulls() {
+		String username = "user1";
+		String password = "password1";
+		addTestUserToDatabase(1, username, password);
+		String username2 = "user2";
+		String password2 = "password2";
+		addTestUserToDatabase(2, username2, password2);
+
+		assertThat(userRepository.findUserByUsernameAndPassword(null, password2)).isNull();
+		assertThat(userRepository.findUserByUsernameAndPassword(username2, null)).isNull();
+		assertThat(userRepository.findUserByUsernameAndPassword(null, null)).isNull();
+	}
+
+	@Test
+	public void testFindUserByUsernameOnExistingUser() {
+		String username = "user1";
+		String password = "password1";
+		addTestUserToDatabase(1, username, password);
+		String username2 = "user2";
+		String password2 = "password2";
+		addTestUserToDatabase(2, username2, password2);
+
+		User user = userRepository.findUserByUsername(username2);
+
+		assertThat(user).isEqualTo(new User(username2, password2, 2));
+		assertThat(user.getPassword()).isEqualTo(password2);
+		assertThat(userRepository.findUserByUsername("")).isNull();
+	}
+
+	@Test
+	public void testFindUserByUsernameWithNull() {
+		String username = "user1";
+		String password = "password1";
+		addTestUserToDatabase(1, username, password);
+
+		assertThat(userRepository.findUserByUsername(null)).isNull();
+	}
+	
+	// docs
+	@Test
+	public void testFindUserByNonExistingUsername() {
+		assertThat(userRepository.findUserByUsername("nonExistent")).isNull();
+	}
+}
