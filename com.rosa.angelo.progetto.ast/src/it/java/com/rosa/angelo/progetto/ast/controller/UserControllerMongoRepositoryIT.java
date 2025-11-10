72077ab67ad6569f35bdc9825dda81ab9afd0fc8
virgula <angelo.rosa@protonmail.com>
2025-11-10 15:39:04 +0100

    integration test: UserControllerMongoRepositoryIT

---
+++ com.rosa.angelo.progetto.ast/src/it/java/com/rosa/angelo/progetto/ast/controller/UserControllerMongoRepositoryIT.java
@@ -0,0 +1,92 @@
+package com.rosa.angelo.progetto.ast.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testcontainers.containers.GenericContainer;
+
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoDatabase;
+import com.rosa.angelo.progetto.ast.model.User;
+import com.rosa.angelo.progetto.ast.repository.UserMongoRepository;
+import com.rosa.angelo.progetto.ast.view.LoginView;
+
+public class UserControllerMongoRepositoryIT {
+
+	@Mock
+	private LoginView loginView;
+
+	private UserMongoRepository userRepository;
+
+	private UserController userController;
+
+	private AutoCloseable closeable;
+
+	private final String validToken = UserMongoRepository.REGISTRATION_TOKEN;
+
+	@SuppressWarnings({ "rawtypes", "resource" })
+	@ClassRule
+	public static final GenericContainer mongo = new GenericContainer(
+			UserMongoRepository.IMAGE + ":" + UserMongoRepository.VERSION).withExposedPorts(UserMongoRepository.PORT);
+	private static MongoClient client;
+	private static MongoDatabase database;
+
+	@BeforeClass
+	public static void beforeClassSetup() {
+		client = new MongoClient(new ServerAddress(mongo.getHost(), mongo.getMappedPort(UserMongoRepository.PORT)));
+		database = client.getDatabase(UserMongoRepository.AST_DB_NAME);
+	}
+
+	@Before
+	public void beforeSetup() {
+		closeable = MockitoAnnotations.openMocks(this);
+		userRepository = new UserMongoRepository(client);
+		database.drop();
+		userController = new UserController(loginView, userRepository);
+	}
+
+	@After
+	public void afterTest() throws Exception {
+		closeable.close();
+		database.drop();
+	}
+
+	@AfterClass
+	public static void tearDown() throws Exception {
+		client.close();
+	}
+
+	@Test
+	public void testLoginControllerITSuccesfullWithExistingUser() {
+		User user = new User("test", "password1234", 1);
+		userRepository.save(user);
+
+		userController.login("test", "password1234");
+
+		verify(loginView, times(1)).switchPanel();
+		verify(loginView, times(0)).showError(anyString());
+		verify(loginView, times(0)).showError(anyString(), any());
+	}
+
+	@Test
+	public void testNewUserController() {
+		User user = new User("test", "password1234", 1);
+		userController.newUser(user, validToken);
+
+		assertThat(userRepository.findUserById(1)).isEqualTo(user);
+		assertThat(userRepository.findUserByUsername("test")).isEqualTo(user);
+		assertThat(userRepository.findUserByUsernameAndPassword("test", "password1234")).isEqualTo(user);
+	}
+}
