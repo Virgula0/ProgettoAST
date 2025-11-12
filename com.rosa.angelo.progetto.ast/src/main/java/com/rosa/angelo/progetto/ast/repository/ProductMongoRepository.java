f14ee06ba722007540971d5a6894cf331814be9f
Angelo Rosa <angelo.rosa@protonmail.com>
2025-11-12 18:22:26 +0100

    Implement mongodb product repository (#26)
    * TDD: findAllProductsSentByUser
    
    * refactor using lambda for findAllProductsSentByUser
    
    * bug: refactor into lambda did not used filter, spotted using testFindAllProductsSentByUserWhenUserIsNull tdd test. solving.
    
    * TDD: testFindAllProductsSentByUserWhenUserIsNull
    
    * TDD: testSaveNewProductInDBIsSuccesfull
    
    * TDD: testSaveNewProductInDBIsNotSuccesfullWhenEmbeddedUserIsNull
    
    * TDD: testSaveNewProductInDBIsNotSuccesfullWhenProductIsNull
    
    * TDD: testDeleteProduct
    
    * TDD: testDeleteNullProduct
    
    * TDD: testFindProductByID
    
    * refactor findProductById into lambda
    
    * refactor db key name for cleareness purposes, apply sonarcube suggestions
    
    * sonarcube suggestion
    
    * sonarcube suggestions
    
    * fix survival mutation testin on newProduct in ProductController

---
+++ com.rosa.angelo.progetto.ast/src/main/java/com/rosa/angelo/progetto/ast/repository/ProductMongoRepository.java
@@ -0,0 +1,97 @@
+package com.rosa.angelo.progetto.ast.repository;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.StreamSupport;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.rosa.angelo.progetto.ast.model.Product;
+import com.rosa.angelo.progetto.ast.model.User;
+
+public class ProductMongoRepository implements ProductRepository {
+	public static final String IMAGE = System.getProperty("mongo.image", "mongo");
+	public static final String VERSION = System.getProperty("mongo.version", "4.4.3");
+	public static final int PORT = Integer.parseInt(System.getProperty("mongo.port", "27017"));
+
+	public static final String AST_DB_NAME = "ast";
+	public static final String PRODUCT_COLLECTION_NAME = "product";
+	private MongoCollection<Document> productCollection;
+
+	public ProductMongoRepository(MongoClient client, String databaseName, String collectionName) {
+		productCollection = client.getDatabase(databaseName).getCollection(collectionName);
+	}
+
+	public ProductMongoRepository(MongoClient client) {
+		this(client, AST_DB_NAME, PRODUCT_COLLECTION_NAME);
+	}
+
+	public static final String SENDER_ID_KEY = "senderid";
+	public static final String SENDER_USERNAME_KEY = "senderusername";
+
+	public static final String PRODUCT_ID_KEY = "id";
+	public static final String RECEIVER_NAME_KEY = "receivername";
+	public static final String RECEIVER_SURNAME_KEY = "receiverusername";
+	public static final String RECEIVER_ADDRESS_KEY = "receiveraddress";
+	public static final String RECEIVER_PACKAGETYPE_KEY = "packagetype";
+
+	private Product documentToProduct(User user, Document doc) {
+		return new Product(user, doc.getString(RECEIVER_NAME_KEY), doc.getString(RECEIVER_SURNAME_KEY),
+				doc.getString(RECEIVER_ADDRESS_KEY), doc.getString(RECEIVER_PACKAGETYPE_KEY),
+				doc.getInteger(PRODUCT_ID_KEY));
+	}
+
+	@Override
+	public List<Product> findAllProductsSentByUser(User user) {
+		if (user == null) {
+			return Collections.emptyList();
+		}
+		return StreamSupport.stream(productCollection.find().spliterator(), false)
+				.filter(x -> Objects.equals(x.getInteger(SENDER_ID_KEY), user.getId()))
+				.map(d -> documentToProduct(user, d)).toList();
+	}
+
+	@Override
+	public void save(Product product) {
+		if (product == null || product.getSender() == null) {
+			return;
+		}
+		productCollection
+				.insertOne(new Document().append(ProductMongoRepository.SENDER_ID_KEY, product.getSender().getId())
+						.append(ProductMongoRepository.SENDER_USERNAME_KEY, product.getSender().getUsername())
+						.append(ProductMongoRepository.PRODUCT_ID_KEY, product.getId())
+						.append(ProductMongoRepository.RECEIVER_NAME_KEY, product.getReceiverName())
+						.append(ProductMongoRepository.RECEIVER_SURNAME_KEY, product.getReceiverSurname())
+						.append(ProductMongoRepository.RECEIVER_ADDRESS_KEY, product.getReiceiverAddress())
+						.append(ProductMongoRepository.RECEIVER_PACKAGETYPE_KEY, product.getPackageType()));
+	}
+
+	@Override
+	public void delete(Product product) {
+		if (product == null) {
+			return;
+		}
+		Document query = new Document(PRODUCT_ID_KEY, product.getId());
+		productCollection.findOneAndDelete(query);
+	}
+
+	@Override
+	public Product findProductById(int id) {
+		return StreamSupport.stream(productCollection.find().spliterator(), false)
+				.filter(x -> Objects.equals(x.getInteger(ProductMongoRepository.PRODUCT_ID_KEY), id))
+				.map(d -> documentToFullUser(d, id)).findFirst().orElse(null);
+	}
+
+	private Product documentToFullUser(Document d, int productId) {
+		return new Product(
+				new User(d.getString(ProductMongoRepository.SENDER_USERNAME_KEY), null,
+						d.getInteger(ProductMongoRepository.SENDER_ID_KEY)),
+				d.getString(ProductMongoRepository.RECEIVER_NAME_KEY),
+				d.getString(ProductMongoRepository.RECEIVER_SURNAME_KEY),
+				d.getString(ProductMongoRepository.RECEIVER_ADDRESS_KEY),
+				d.getString(ProductMongoRepository.RECEIVER_PACKAGETYPE_KEY), productId);
+	}
+}
---
+++ com.rosa.angelo.progetto.ast/src/test/java/com/rosa/angelo/progetto/ast/repository/TestProductMongoDBRepository.java
@@ -0,0 +1,171 @@
+package com.rosa.angelo.progetto.ast.repository;
+
+import static com.mongodb.client.model.Filters.eq;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+
+import org.bson.Document;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.rosa.angelo.progetto.ast.model.Product;
+import com.rosa.angelo.progetto.ast.model.User;
+
+public class TestProductMongoDBRepository {
+
+	@SuppressWarnings({ "rawtypes", "resource" })
+	@ClassRule
+	public static final GenericContainer mongo = new GenericContainer(
+			ProductMongoRepository.IMAGE + ":" + ProductMongoRepository.VERSION)
+			.withExposedPorts(ProductMongoRepository.PORT);
+
+	private static MongoClient client;
+	private static MongoDatabase database;
+
+	private ProductMongoRepository productRepository;
+	private MongoCollection<Document> productCollection;
+
+	private User loggedInUser;
+
+	@BeforeClass
+	public static void beforeClass() {
+		client = new MongoClient(new ServerAddress(mongo.getHost(), mongo.getMappedPort(ProductMongoRepository.PORT)));
+		database = client.getDatabase(ProductMongoRepository.AST_DB_NAME);
+	}
+
+	@Before
+	public void setup() {
+		// make sure we always start with a clean database
+		database.drop();
+		loggedInUser = new User("test", "password1234", 1);
+		productRepository = new ProductMongoRepository(client);
+		productCollection = database.getCollection(ProductMongoRepository.PRODUCT_COLLECTION_NAME);
+	}
+
+	private void addTestProductToDatabase(Product p) {
+		productCollection.insertOne(new Document().append(ProductMongoRepository.SENDER_ID_KEY, p.getSender().getId())
+				.append(ProductMongoRepository.SENDER_USERNAME_KEY, p.getSender().getUsername())
+				.append(ProductMongoRepository.PRODUCT_ID_KEY, p.getId())
+				.append(ProductMongoRepository.RECEIVER_NAME_KEY, p.getReceiverName())
+				.append(ProductMongoRepository.RECEIVER_SURNAME_KEY, p.getReceiverSurname())
+				.append(ProductMongoRepository.RECEIVER_ADDRESS_KEY, p.getReiceiverAddress())
+				.append(ProductMongoRepository.RECEIVER_PACKAGETYPE_KEY, p.getPackageType()));
+	}
+
+	private Product documentToProduct(User user, Document doc) {
+		return new Product(user, doc.getString(ProductMongoRepository.RECEIVER_NAME_KEY),
+				doc.getString(ProductMongoRepository.RECEIVER_SURNAME_KEY),
+				doc.getString(ProductMongoRepository.RECEIVER_ADDRESS_KEY),
+				doc.getString(ProductMongoRepository.RECEIVER_PACKAGETYPE_KEY),
+				doc.getInteger(ProductMongoRepository.PRODUCT_ID_KEY));
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		client.close();
+	}
+
+	@Test
+	public void testFindAllProductsSentByUser() {
+		Product p1 = new Product(loggedInUser, "test", "test", "testAddress", "testPackage", 1);
+		Product p2 = new Product(loggedInUser, "test2", "test2", "testAddress2", "testPackage2", 2);
+		Product p3 = new Product(new User("anotherUser", "password1234", 2), "test2", "test2", "testAddress2",
+				"testPackage2", 2);
+
+		addTestProductToDatabase(p1);
+		addTestProductToDatabase(p2);
+		addTestProductToDatabase(p3);
+
+		assertThat(productRepository.findAllProductsSentByUser(loggedInUser)).containsExactly(p1, p2);
+	}
+
+	@Test
+	public void testFindAllProductsSentByUserWhenUserIsNull() {
+		Product p1 = new Product(loggedInUser, "test", "test", "testAddress", "testPackage", 1);
+		Product p2 = new Product(loggedInUser, "test2", "test2", "testAddress2", "testPackage2", 2);
+
+		addTestProductToDatabase(p1);
+		addTestProductToDatabase(p2);
+		assertThat(productRepository.findAllProductsSentByUser(null)).isEmpty();
+	}
+
+	@Test
+	public void testSaveNewProductInDBIsSuccesfull() {
+		Product p1 = new Product(loggedInUser, "test", "test", "testAddress", "testPackage", 1);
+		productRepository.save(p1);
+
+		assertThat(productCollection.find(eq(ProductMongoRepository.PRODUCT_ID_KEY, p1.getId()))
+				.map(doc -> documentToProduct(loggedInUser, doc)).into(new ArrayList<>())).containsExactly(p1);
+	}
+
+	@Test
+	public void testSaveNewProductInDBIsNotSuccesfullWhenEmbeddedUserIsNull() {
+		Product p1 = new Product(null, "test", "test", "testAddress", "testPackage", 1);
+		productRepository.save(p1);
+
+		assertThat(productCollection.find(eq(ProductMongoRepository.PRODUCT_ID_KEY, p1.getId()))
+				.map(doc -> documentToProduct(loggedInUser, doc)).into(new ArrayList<>())).isEmpty();
+	}
+
+	@Test
+	public void testSaveNewProductInDBIsNotSuccesfullWhenProductIsNull() {
+		Product p1 = null;
+		productRepository.save(p1);
+
+		assertThat(productCollection.find(eq(ProductMongoRepository.PRODUCT_ID_KEY, null))
+				.map(doc -> documentToProduct(loggedInUser, doc)).into(new ArrayList<>())).isEmpty();
+	}
+
+	@Test
+	public void testDeleteProduct() {
+		Product p1 = new Product(loggedInUser, "test", "test", "testAddress", "testPackage", 1);
+		Product p2 = new Product(loggedInUser, "test", "test", "testAddress", "testPackage", 1);
+
+		addTestProductToDatabase(p1);
+		addTestProductToDatabase(p2);
+
+		assertThat(productCollection.find(eq(ProductMongoRepository.PRODUCT_ID_KEY, p1.getId()))
+				.map(doc -> documentToProduct(loggedInUser, doc)).into(new ArrayList<>())).containsExactly(p1, p2);
+
+		productRepository.delete(p1);
+
+		assertThat(productCollection.find(eq(ProductMongoRepository.PRODUCT_ID_KEY, p1.getId()))
+				.map(doc -> documentToProduct(loggedInUser, doc)).into(new ArrayList<>())).containsExactly(p2);
+	}
+
+	@Test
+	public void testDeleteNullProduct() {
+		Product p1 = new Product(loggedInUser, "test", "test", "testAddress", "testPackage", 1);
+		addTestProductToDatabase(p1);
+
+		assertThat(productCollection.find(eq(ProductMongoRepository.PRODUCT_ID_KEY, p1.getId()))
+				.map(doc -> documentToProduct(loggedInUser, doc)).into(new ArrayList<>())).containsExactly(p1);
+
+		productRepository.delete(null);
+
+		assertThat(productCollection.find(eq(ProductMongoRepository.PRODUCT_ID_KEY, p1.getId()))
+				.map(doc -> documentToProduct(loggedInUser, doc)).into(new ArrayList<>())).containsExactly(p1);
+	}
+
+	@Test
+	public void testFindProductByID() {
+		Product p1 = new Product(loggedInUser, "test", "test", "testAddress", "testPackage", 1);
+		Product p2 = new Product(loggedInUser, "test2", "test2", "testAddress2", "testPackage2", 2);
+
+		addTestProductToDatabase(p1);
+		addTestProductToDatabase(p2);
+
+		assertThat(productRepository.findProductById(p1.getId())).isEqualTo(p1);
+		assertThat(productRepository.findProductById(-1)).isNull();
+
+	}
+}
